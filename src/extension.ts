b56705fc302989ad87141788855ecdac6c68142f
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-01-18 22:44:33 +0530

    [remote] Add openExternal command

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,18 @@
 import * as vscode from 'vscode';
+import { Server } from 'remote-adb';
+
+let server: Server;
+let serverUri: vscode.Uri;
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('remote-android.startServer', () => {
-		// Start the remote-adb server
+	let disposable = vscode.commands.registerCommand('remote-android.openExternal', async () => {
+		if (!server) {
+			// Start the remote-adb server
+			server = new Server(0);
+			serverUri = vscode.Uri.parse(await server.start());
+		}
+
+		vscode.env.openExternal(serverUri);
 	});
 
 	context.subscriptions.push(disposable);
