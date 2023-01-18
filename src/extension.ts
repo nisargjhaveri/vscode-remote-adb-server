b67ca82db9dfa00300435682053933a5f703c880
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-01-19 00:12:12 +0530

    [remote] Add command to stop the server

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,26 @@ import { Server, setLogger as setRemoteAdbLogger } from 'remote-adb';
 
 import { OutputLogger } from './logger';
 
-let server: Server;
+let server: Server|undefined = undefined;
 let serverUri: vscode.Uri;
 
 export function activate(context: vscode.ExtensionContext) {
 	setRemoteAdbLogger(new OutputLogger());
 
-	let disposable = vscode.commands.registerCommand('remote-android.openExternal', async () => {
+	context.subscriptions.push(vscode.commands.registerCommand('remote-android.openExternal', async () => {
 		if (!server) {
-			// Start the remote-adb server
 			server = new Server(0);
+		}
+		if (!server.isListening()) {
 			serverUri = vscode.Uri.parse(await server.start());
 		}
 
 		vscode.env.openExternal(serverUri);
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('remote-android.stopServer', async () => {
+		await server?.stop();
+	}));
 }
 
 export function deactivate() {}
