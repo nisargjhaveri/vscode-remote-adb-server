bc57434b436fd0acfc488f88917b48af6c8e1930
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-01-19 23:16:10 +0530

    [remote] Add getExternalUrl command

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,34 @@ import { Server, setLogger as setRemoteAdbLogger } from 'remote-adb';
 import { OutputLogger } from './logger';
 
 let server: Server|undefined = undefined;
-let serverUri: vscode.Uri;
+let serverUri: vscode.Uri|undefined = undefined;
+
+async function ensureServerListening() {
+	if (!server) {
+		server = new Server(0);
+	}
+	if (!server.isListening()) {
+		serverUri = vscode.Uri.parse(await server.start());
+	}
+}
 
 export function activate(context: vscode.ExtensionContext) {
 	setRemoteAdbLogger(new OutputLogger());
 
 	context.subscriptions.push(vscode.commands.registerCommand('remote-android.openExternal', async () => {
-		if (!server) {
-			server = new Server(0);
-		}
-		if (!server.isListening()) {
-			serverUri = vscode.Uri.parse(await server.start());
-		}
+		await ensureServerListening();
 
-		vscode.env.openExternal(serverUri);
+		if (serverUri) {
+			await vscode.env.openExternal(serverUri);
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remote-android.getExternalUrl', async (): Promise<vscode.Uri|undefined> => {
+		await ensureServerListening();
+
+		if (serverUri) {
+			return await vscode.env.asExternalUri(serverUri);
+		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('remote-android.stopServer', async () => {
