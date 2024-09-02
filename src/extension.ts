279ba608b0ca477fbeaf9e82469212d2d54a5344
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2024-09-02 23:17:30 +0530

    Improve package organization, start server on 127.0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,20 @@ import { logger } from './logger';
 let server: Server|undefined = undefined;
 let serverUri: vscode.Uri|undefined = undefined;
 
-async function ensureServerListening() {
+async function ensureServerListening(context: vscode.ExtensionContext) {
 	if (!server) {
-		server = new Server({ port: 0 });
+		server = new Server(
+			{
+				port: 0,
+				host: "127.0.0.1"
+			},
+			/* httpsOptions */ undefined,
+			{
+				staticClientPath: vscode.Uri.joinPath(context.extensionUri, "dist/web").fsPath,
+			}
+		);
 	}
+
 	if (!server.isListening()) {
 		serverUri = vscode.Uri.parse(await server.start());
 	}
@@ -20,7 +30,7 @@ export function activate(context: vscode.ExtensionContext) {
 	setRemoteAdbLogger(logger);
 
 	context.subscriptions.push(vscode.commands.registerCommand('remote-adb.openExternal', async () => {
-		await ensureServerListening();
+		await ensureServerListening(context);
 
 		if (serverUri) {
 			await vscode.env.openExternal(serverUri);
@@ -28,7 +38,7 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('remote-adb.getExternalUrl', async (): Promise<vscode.Uri|undefined> => {
-		await ensureServerListening();
+		await ensureServerListening(context);
 
 		if (serverUri) {
 			return await vscode.env.asExternalUri(serverUri);
